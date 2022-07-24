c7ccd9616ef72ac5faedf27befe2bea1fba9e4a7
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-23 20:44:04 -0700

    Update radiant_node_testing.sh

--- radiant_node_testing.sh
+++ radiant_node_testing.sh
@@ -296,8 +296,11 @@ elif [[ "${nowal_upnp_zmq_qt[*]}" =~ "$radiant_OS" ]]; then
 elif [[ "$uname_OS" == OpenBSD ]]; then
 	cmake -GNinja .. -DBUILD_RADIANT_QT=OFF -DBUILD_BITCOIN_WALLET=OFF
 elif  [[ "$uname_OS" == NetBSD ]]; then
+	export CMAKE_C_COMPILER=/usr/pkg/gcc9/bin/gcc
+	export CMAKE_CXX_COMPILER=/usr/pkg/gcc9/bin/gcc
 	CC=/usr/pkg/gcc9/bin/gcc CXX=/usr/pkg/gcc9/bin/g++-4.2 cmake -GNinja .. \
-	-DBUILD_RADIANT_QT=OFF -DBUILD_BITCOIN_WALLET=OFF -CMAKE_C_COMPILER=gcc -CMAKE_CXX_COMPILER=g++
+	-DBUILD_RADIANT_QT=OFF -DBUILD_BITCOIN_WALLET=OFF 
+	# -CMAKE_C_COMPILER=gcc -CMAKE_CXX_COMPILER=g++
 fi
 
 debug_step="ninja build"; progress_banner
